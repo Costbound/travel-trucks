807425f879778d92fd4cb4d7f8a90012e41dd948
Dmytro Popovskyi <popovskiy96@gmail.com>
2025-08-30 11:49:24 +0200

    init project

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import '../node_modules/modern-normalize/modern-normalize.css'
+import '../node_modules/reset-css/reset.css'
+import App from './components/App/App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
